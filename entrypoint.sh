7f3d76ae3906a184ab15ef6cdf50b49ef257f503
Stig Otnes Kolstad <stig.otnes.kolstad@nrk.no>
2022-02-08 21:06:14 +0100

    feat: rewrite entrypoint for v4 support
    BREAKING CHANGE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,54 @@
 #!/bin/bash
 set -eux
 
-strict=''
-schema=${INPUT_SCHEMA:-$1}
-target=${INPUT_TARGET:-$2}
+# Returns a string `true` the string is considered boolean true,
+# otherwise `false`. An empty value is considered false.
+function str_bool {
+  local str="${1:-false}"
+  local pat='^(true|1|yes)$'
+  if [[ "$str" =~ $pat ]]
+  then
+    echo 'true'
+  else
+    echo 'false'
+  fi
+}
 
-if [ -n "${INPUT_STRICT:-}" ]
-then
-  strict='--strict'
-fi
+schema="$INPUT_SCHEMA"
+target="$INPUT_TARGET"
+no_strict=$(str_bool "${INPUT_NO_STRICT:-}")
+error_is_success=$(str_bool "${INPUT_ERROR_IS_SUCCESS:-}")
 
-if [ ! -e ${schema} ]
+# Must end with a space here
+extra_args=' '
+
+if [ ! -e "${schema}" ]
 then
   >&2 echo "Schema does not exist: $schema"
   exit 1
 fi
 
-if [ ! -e ${target} ]
+# TODO: Allow directories
+if [ ! -e "${target}" ]
 then
   >&2 echo "Target does not exist: $target"
   exit 1
 fi
 
-yamale --schema=${schema} $target $strict
+if [ "$no_strict" = "true" ]
+then
+  extra_args='--no-strict '
+fi
+
+if [ "$error_is_success" = "true" ]
+then
+  # Flipped validation logic
+  echo "--- Flipped validation logic enabled (error-is-success: true)! ---"
+  # shellcheck disable=SC2086
+  yamale $extra_args --schema="${schema}" "$target" && exit 1
+  exit 0
+fi
+
+# Normal execution
+# shellcheck disable=SC2086
+yamale $extra_args --schema="${schema}" "$target"
